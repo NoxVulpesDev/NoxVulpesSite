67343627dedeaee497ac2f08543841089926ab65
NoxVulpesDev <53783377+NoxVulpesDev@users.noreply.github.com>
2020-05-20 11:50:44 +0100

    Add files via upload

--- simple webpage exammple/script.js
+++ simple webpage exammple/script.js
@@ -7,18 +7,3 @@ function checkTest(){
     shipAdd.className = 'invisible';
   }
 };
-
-pca.on("options", function(type, key, options){
-if(type == "capture+"){
-options.suppressAutocomplete = false;
-}
-});
-
-pca.on("load", function(type, id, control){
-var isChrome = !!window.chrome;
-if (isChrome == true){
-for (i=0; control.fields.length > i; i++){
-document.getElementById(control.fields[i].element).autocomplete = "PCATEST";
-};
-};
-});
