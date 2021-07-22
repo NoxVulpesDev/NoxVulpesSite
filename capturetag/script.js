adcda60bc60fe338a4a88fab62b7d140d0ac9ca9
NoxVulpesDev <53783377+NoxVulpesDev@users.noreply.github.com>
2021-07-22 15:43:43 +0100

    Add files via upload

---
+++ capturetag/script.js
@@ -0,0 +1,11 @@
+
+function checkTest(){
+  var shipAdd = document.getElementById('shippingAddress');
+  if (shipAdd.className === 'invisible'){
+    shipAdd.className = 'visible';
+  } else{
+    shipAdd.className = 'invisible';
+  }
+};
+
+(function(n,t,i,r){var u,f;n[i]=n[i]||{},n[i].initial={accountCode:"POSTC11867",host:"POSTC11867.pcapredict.com"},n[i].on=n[i].on||function(){(n[i].onq=n[i].onq||[]).push(arguments)},u=t.createElement("script"),u.async=!0,u.src=r,f=t.getElementsByTagName("script")[0],f.parentNode.insertBefore(u,f)})(window,document,"pca","//POSTC11867.pcapredict.com/js/sensor.js");
