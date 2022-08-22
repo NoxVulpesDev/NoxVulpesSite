02dbae579dd6e96a5905d56c05ff36a9e13e7863
NoxVulpesDev <53783377+NoxVulpesDev@users.noreply.github.com>
2022-08-22 16:19:05 +0100

    Update script.js

--- capturetag/script.js
+++ capturetag/script.js
@@ -8,4 +8,4 @@ function checkTest(){
   }
 };
 
-(function(n,t,i,r){var u,f;n[i]=n[i]||{},n[i].initial={accountCode:"POSTC11867",host:"POSTC11867.pcapredict.com"},n[i].on=n[i].on||function(){(n[i].onq=n[i].onq||[]).push(arguments)},u=t.createElement("script"),u.async=!0,u.src=r,f=t.getElementsByTagName("script")[0],f.parentNode.insertBefore(u,f)})(window,document,"pca","//POSTC11867.pcapredict.com/js/sensor.js");
+(function(n,t,i,r){var u,f;n[i]=n[i]||{},n[i].initial={accountCode:"POSTC11867",host:"POSTC11867.pcapredict.com"},n[i].on=n[i].on||function(){(n[i].onq=n[i].onq||[]).push(arguments)},u=t.createElement("script"),u.async=!0,u.src=r,f=t.getElementsByTagName("script")[0],f.parentNode.insertBefore(u,f)})(window,document,"pca","//POSTC11867.pcapredict.com/js/sensor.js")
